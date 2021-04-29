29f8c916ef1d18ba14ccb6c9a60b9beefb366771
Kush Daga <kushdaga@hotmail.com>
2021-04-29 23:25:55 +0530

    changed duraiton:

--- devcon/src/components/HamburgerMenu/HamburgerMenu.styles.js
+++ devcon/src/components/HamburgerMenu/HamburgerMenu.styles.js
@@ -3,63 +3,63 @@ import styled from "styled-components";
 import { lg, md, xl } from "../../theme/breakpoints";
 
 export const Menu = styled.div`
-  display: flex;
-  flex-direction: column;
-  background-color: ${({ theme }) => theme.text};
-  min-height: 100vh;
-  width: 100vw;
-  position: fixed;
-  z-index: 100;
-  overflow-y: hidden;
-  top: 0;
-  left: 0;
-  transform: ${(props) =>
-    props.isOpen === true
-      ? "translate3d(0, 0, 0)"
-      : "translate3d(100vw, 0, 0)"};
-  transition: transform 225ms cubic-bezier(0, 0, 0.2, 1) 0ms;
+	display: flex;
+	flex-direction: column;
+	background-color: ${({ theme }) => theme.text};
+	min-height: 100vh;
+	width: 100vw;
+	position: fixed;
+	z-index: 100;
+	overflow-y: hidden;
+	top: 0;
+	left: 0;
+	transform: ${(props) =>
+		props.isOpen === true
+			? "translate3d(0, 0, 0)"
+			: "translate3d(100vw, 0, 0)"};
+	transition: transform 500ms cubic-bezier(0, 0, 0.2, 1) 0ms;
 
-  ${lg},
-  ${xl} {
-    display: none;
-  }
-  ${md} {
-    display: flex;
-  }
+	${lg},
+	${xl} {
+		display: none;
+	}
+	${md} {
+		display: flex;
+	}
 `;
 
 export const MenuItem = styled.div`
-  padding: 1rem;
-  color: #000;
-  width: fit-content;
-  font-size: 1.6rem;
-  text-decoration: none;
+	padding: 1rem;
+	color: #000;
+	width: fit-content;
+	font-size: 1.6rem;
+	text-decoration: none;
 `;
 
 export const LogoSection = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  padding: 2rem 1rem;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	padding: 2rem 1rem;
 
-  .heading {
-    font-size: 2rem;
-    font-weight: bold;
-    color: #000;
-    margin: 0;
-  }
+	.heading {
+		font-size: 2rem;
+		font-weight: bold;
+		color: #000;
+		margin: 0;
+	}
 
-  .close {
-    fill: #000;
-    font-size: 1.2rem;
-  }
+	.close {
+		fill: #000;
+		font-size: 1.2rem;
+	}
 `;
 
 export const MenuSection = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  flex-grow: 1;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	flex-grow: 1;
 `;
--- devcon/src/components/Hero/Hero.styles.js
+++ devcon/src/components/Hero/Hero.styles.js
@@ -3,133 +3,133 @@ import styled from "styled-components";
 import { lg, md, xs } from "../../theme/breakpoints";
 
 export const HeroSection = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-self: center;
-  justify-content: center;
-  align-items: center;
+	display: flex;
+	flex-direction: column;
+	align-self: center;
+	justify-content: center;
+	align-items: center;
 `;
 
 export const PrimaryTitle = styled.h1`
-  font-size: 210px;
-  line-height: 254px;
-  font-weight: 800;
-  margin: 0;
-  /* color: #3dbfe8; */
-  background: -webkit-linear-gradient(180deg, #3dbfe8 19.71%, #14319b 100%);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
+	font-size: 210px;
+	line-height: 254px;
+	font-weight: 800;
+	margin: 0;
+	/* color: #3dbfe8; */
+	background: -webkit-linear-gradient(180deg, #3dbfe8 19.71%, #14319b 100%);
+	-webkit-background-clip: text;
+	-webkit-text-fill-color: transparent;
 
-  ${lg} {
-    font-size: 180px;
-    line-height: 224px;
-  }
+	${lg} {
+		font-size: 180px;
+		line-height: 224px;
+	}
 
-  ${md} {
-    font-size: 8rem;
-    line-height: 10rem;
-  }
+	${md} {
+		font-size: 8rem;
+		line-height: 10rem;
+	}
 
-  ${xs} {
-    font-size: 4rem;
-    line-height: 5rem;
-  }
+	${xs} {
+		font-size: 4rem;
+		line-height: 5rem;
+	}
 `;
 
 export const SecondaryTitle = styled.h3`
-  font-size: 52px;
-  line-height: 63px;
-  font-weight: 500;
-  padding: 0 2rem;
-  letter-spacing: 0.37em;
-  margin: 0;
+	font-size: 52px;
+	line-height: 63px;
+	font-weight: 500;
+	padding: 0 2rem;
+	letter-spacing: 0.37em;
+	margin: 0;
 
-  ${lg} {
-    font-size: 48px;
-    line-height: 56px;
-  }
+	${lg} {
+		font-size: 48px;
+		line-height: 56px;
+	}
 
-  ${md} {
-    font-size: 2rem;
-    line-height: 5rem;
-  }
+	${md} {
+		font-size: 2rem;
+		line-height: 5rem;
+	}
 
-  ${xs} {
-    font-size: 1rem;
-    line-height: 3rem;
-  }
+	${xs} {
+		font-size: 1rem;
+		line-height: 3rem;
+	}
 `;
 
 export const ActionBar = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  margin: 2rem 0;
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	margin: 2rem 0;
 `;
 
 export const ActionButton = styled.button`
-  padding: 0.8rem 2.2rem;
-  border: ${({ theme }) => `1px solid ${theme.text}`};
-  color: ${({ theme }) => theme.text};
-  background-color: transparent;
-  border-radius: 6px;
-  font-size: 1.1rem;
-  line-height: 1.2rem;
-  width: 190px;
-  overflow: hidden;
-  position: relative;
-  cursor: pointer;
+	padding: 0.8rem 2.2rem;
+	border: ${({ theme }) => `1px solid ${theme.text}`};
+	color: ${({ theme }) => theme.text};
+	background-color: transparent;
+	border-radius: 6px;
+	font-size: 1.1rem;
+	line-height: 1.2rem;
+	width: 190px;
+	overflow: hidden;
+	position: relative;
+	cursor: pointer;
 
-  &:focus {
-    outline: none;
-  }
+	&:focus {
+		outline: none;
+	}
 
-  &::after {
-    background: #fff;
-    content: "";
-    height: 155px;
-    left: -75px;
-    opacity: 0.2;
-    position: absolute;
-    top: -50px;
-    transform: rotate(35deg);
-    transition: all 550ms cubic-bezier(0.19, 1, 0.22, 1);
-    width: 50px;
-    z-index: -10;
-  }
+	&::after {
+		background: #fff;
+		content: "";
+		height: 155px;
+		left: -75px;
+		opacity: 0.2;
+		position: absolute;
+		top: -50px;
+		transform: rotate(35deg);
+		transition: all 550ms cubic-bezier(0.19, 1, 0.22, 1);
+		width: 50px;
+		z-index: -10;
+	}
 
-  &:hover::after {
-    left: 120%;
-    transition: all 550ms cubic-bezier(0.19, 1, 0.22, 1);
-  }
+	&:hover::after {
+		left: 120%;
+		transition: all 550ms cubic-bezier(0.19, 1, 0.22, 1);
+	}
 
-  &:last-of-type {
-    margin-left: 2rem;
-  }
+	&:last-of-type {
+		margin-left: 2rem;
+	}
 
-  ${xs} {
-    width: 50%;
-  }
+	${xs} {
+		width: 50%;
+	}
 `;
 
 export const StyledVideo = styled.video`
-  margin: 6rem 0;
-  box-shadow: rgb(0, 0, 0) 0px 0px 160px;
-  border-radius: 20px;
-  width: 1078px;
-  object-fit: contain;
+	margin: 6rem 0;
+	box-shadow: rgb(0, 0, 0) 0px 0px 50px;
+	border-radius: 20px;
+	width: 1078px;
+	object-fit: contain;
 
-  ${lg} {
-    margin: 4rem 0;
-  }
+	${lg} {
+		margin: 4rem 0;
+	}
 
-  ${md} {
-    width: 95%;
-    box-shadow: rgb(0, 0, 0) 0px 0px 2rem;
-  }
+	${md} {
+		width: 95%;
+		box-shadow: rgb(0, 0, 0) 0px 0px 50px;
+	}
 
-  ${xs} {
-    width: 80%;
-    margin: 2rem 0;
-  }
+	${xs} {
+		width: 80%;
+		margin: 2rem 0;
+	}
 `;
