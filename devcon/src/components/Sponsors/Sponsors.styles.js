30700976c570753cde635cd2fab4ebc721be532a
aniketbiswas21 <aniket.biswas75@gmail.com>
2021-06-07 16:24:48 +0530

    added sponsors

--- devcon/src/components/Sponsors/Sponsors.styles.js
+++ devcon/src/components/Sponsors/Sponsors.styles.js
@@ -1,92 +1,109 @@
 import styled from "styled-components";
-import { md, xs } from "../../theme/breakpoints";
+import { md, sm, xs } from "../../theme/breakpoints";
 
 export const SponsorsSection = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	margin: 3rem 0;
-	padding: 1.2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 3rem 0;
+  padding: 1.2rem;
 `;
 export const HeadingDiv = styled.div`
-	font-weight: 800;
-	font-size: 120px;
+  font-weight: 800;
+  font-size: 120px;
 
-	color: white;
-	margin: 0 0 3rem 0;
+  color: white;
+  margin: 0 0 1.5rem 0;
 
-	${md} {
-		font-size: 6rem;
-		padding: 0px 10px;
-	}
+  ${md} {
+    font-size: 6rem;
+    padding: 0px 10px;
+  }
 
-	${xs} {
-		font-size: 3rem;
-		line-height: 3rem;
-	}
+  ${xs} {
+    font-size: 3rem;
+    line-height: 3rem;
+  }
 `;
 
 export const Heading = styled.h1`
-	color: white;
-	margin: 0px 0px;
-	font-size: 120px;
-	${md} {
-		font-size: 6rem;
-	}
+  color: white;
+  margin: 0px 0px;
+  font-size: 120px;
+  ${md} {
+    font-size: 6rem;
+  }
 
-	${xs} {
-		font-size: 3rem;
-	}
+  ${xs} {
+    font-size: 3rem;
+  }
 `;
 export const SubHeading = styled.h6`
-	color: #b5b5b5;
-	margin: 1.5rem 0px;
-	font-size: 38px;
-	${md} {
-		font-size: 2rem;
-		// padding: 1rem;
-	}
+  color: #b5b5b5;
+  margin: 1.5rem 0px;
+  font-size: 38px;
+  ${md} {
+    font-size: 2rem;
+    // padding: 1rem;
+  }
 
-	${xs} {
-		font-size: 1.5rem;
-		// line-height: 4rem;
-		// padding: 1rem;
-	}
+  ${xs} {
+    font-size: 1.5rem;
+    // line-height: 4rem;
+    // padding: 1rem;
+  }
 `;
 
 export const SponsorTitle = styled.h6`
-	color: white;
-	margin: 10px 0px 30px 0px;
-	font-size: 72px;
+  color: white;
+  margin: 10px 0px 30px 0px;
+  font-size: 70px;
 
-	${md} {
-		font-size: 3rem;
-		padding: 0px 10px;
-	}
+  ${md} {
+    font-size: 3rem;
+    padding: 0px 10px;
+  }
 
-	${xs} {
-		font-size: 2rem;
-		// line-height: 4rem;
-	}
+  ${xs} {
+    font-size: 2rem;
+    // line-height: 4rem;
+  }
 `;
 
 export const SponsorSectionDiv = styled.div`
-	margin: 30px 0px;
-	${md} {
-		margin-top: 0px;
-	}
+  margin: 0px 0px 3rem 0px;
+
+  ${md} {
+    margin-top: 0px;
+  }
 `;
 
 export const SponsorCardSection = styled.div`
-	display: flex;
-	flex-wrap: wrap;
-	// justify-content: ;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+
+  ${xs} {
+    justify-content: center;
+  }
 `;
-export const Card = styled.div`
-	flex: 1 0 25%;
-	// width: 352px;
-	height: 105px;
-	margin: 10px 20px;
-	background-color: #c4c4c4;
-	border-radius: 24px;
+
+export const SponsorImgContainer = styled.div`
+  border: 2px solid #fff;
+  border-radius: 10px;
+  padding: 1.5rem;
+
+  ${xs} {
+    margin-bottom: 1rem;
+  }
+`;
+
+export const SponsorImg = styled.img`
+  height: 70px;
+  object-fit: contain;
+  margin: 1rem 1rem;
+
+  ${sm} {
+    height: 3rem;
+  }
 `;
